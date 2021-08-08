49351b3e8eecc836303bc03f5286d967a629dbb7
Ted S <tstaros23@gmail.com>
2021-08-07 21:14:03 -0600

    Create shift keys

--- lib/key.rb
+++ lib/key.rb
@@ -1,11 +1,17 @@
 class Key
-  def create_keys(rand_num)
+  def initialize
+
+  end
+  def create_shift_keys(key)
     hash_keys = %i[A B C D]
-    digit_sets = [rand_num[0..1], rand_num[1..2], rand_num[2..3], rand_num[3..4]]
+    digit_sets = [key[0..1], key[1..2], key[2..3], key[3..4]]
     values = digit_sets.map do |set|
-      set.join.to_i
+      set.to_i
     end
     keys = hash_keys.zip(values).to_h
-    # require "pry"; binding.pry
+  end
+
+  def generate_key
+    
   end
 end
--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -2,7 +2,7 @@ require './lib/key'
 require 'date'
 
 RSpec.describe Key do
-  it "exists " do
+  xit "exists " do
     key = Key.new
 
     expect(key).to be_a(Key)
@@ -10,9 +10,9 @@ RSpec.describe Key do
   it "can create keys" do
     key = Key.new
 
-    expect(key.create_keys([1, 2, 3, 4, 5])).to eq(
+    expect(key.create_shift_keys("02715")).to eq(
       {
-        :A => 02,
+        :A => 2,
         :B => 27,
         :C => 71,
         :D => 15
