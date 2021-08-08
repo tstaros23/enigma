71afafa6463e2d2e3ca374545e1bd708c47feb7f
Ted S <tstaros23@gmail.com>
2021-08-08 11:48:37 -0600

    Finish create offset method

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,3 +1,12 @@
 class Offset
-  
+  def create_offset(date)
+    hash_keys = %i[A B C D]
+    numeric_form = date ** 2
+    last_four_numbers = numeric_form.to_s.chars.last(4)
+    digits = last_four_numbers.map do |num|
+      num.to_i
+    end
+    offsets =  hash_keys.zip(digits).to_h
+    # require "pry"; binding.pry
+  end
 end
--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -6,6 +6,18 @@ RSpec.describe Offset do
   it "exists" do
     offset = Offset.new
 
-    expect(offeset).to be_a(Offset)
+    expect(offset).to be_a(Offset)
+  end
+  it "can create an offset" do
+    offset = Offset.new
+
+    expect(offset.create_offset(40895)).to eq(
+      {
+        :A => 1,
+        :B => 0,
+        :C => 2,
+        :D => 5
+      }
+    )
   end
 end
