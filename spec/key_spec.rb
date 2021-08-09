2e193900f699779efbb69138f60d86e61c67fe90
Ted S <tstaros23@gmail.com>
2021-08-09 17:32:14 -0600

    Refactor key spec

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -3,14 +3,14 @@ require 'date'
 
 RSpec.describe Key do
   it "exists and has attributes" do
-    key = Key.new("02715")
+    key = Key.new
 
     expect(key).to be_a(Key)
   end
   it "can create shift keys" do
-    key = Key.new("02715")
-    allow(key).to receive(:create_shift_keys).and_return({:A => 2, :B => 27, :C => 71, :D => 15})
-    expect(key.create_shift_keys).to eq(
+    key = Key.new
+
+    expect(key.create_shift_keys("02715")).to eq(
       {
         :A => 2,
         :B => 27,
@@ -21,7 +21,7 @@ RSpec.describe Key do
   end
 
   it "can create a key" do
-    key = Key.new("02715")
+    key = Key.new
 
     allow_any_instance_of(Key).to receive(:random_index).and_return(0)
     expect(key.generate_key).to eq("00000")
@@ -29,9 +29,9 @@ RSpec.describe Key do
   end
 
   it "can create a random index" do
-    key = Key.new("02715")
+    key = Key.new
 
-    
+
     expect(key.random_index).to be_between(0, 9)
   end
 end
