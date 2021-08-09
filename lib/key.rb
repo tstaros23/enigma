9f2e8ddc39fe896cf410781200fd15639646d3fd
Ted S <tstaros23@gmail.com>
2021-08-09 17:11:55 -0600

    update key to take in string

--- lib/key.rb
+++ lib/key.rb
@@ -1,11 +1,6 @@
 class Key
-  attr_reader :key
 
-  def initialize(key)
-    @key = key
-  end
-  def create_shift_keys
-    key = generate_key
+  def create_shift_keys(key)
     hash_keys = %i[A B C D]
     digit_sets = [key[0..1], key[1..2], key[2..3], key[3..4]]
     values = digit_sets.map do |set|
