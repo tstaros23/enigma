5bd42d37c27c8acce2824892334a1b2b01f10494
Ted S <tstaros23@gmail.com>
2021-08-09 22:36:33 -0600

    Add decryption method to create hash

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,29 +1,34 @@
 require './lib/key'
 require './lib/offset'
 require './lib/encrypt'
+require './lib/decrypt'
 
 class Enigma
+  def initialize
+    @offset = Offset.new
+    @key_generator = Key.new
+  end
 
   def encrypt(message, provided_key = nil, provided_date = nil)
 
-    offset = Offset.new
-    key_generator = Key.new
-    key = provided_key.nil? ? key_generator.generate_key : provided_key
-    date = provided_date.nil? ? offset.generate_date : provided_date
-    encrypter = Encrypt.new(message, key_generator.create_shift_keys(key) , offset.create_offset(date))
-    encryption = encrypter.encrypt
+
+    key = provided_key.nil? ? @key_generator.generate_key : provided_key
+    date = provided_date.nil? ? @offset.generate_date : provided_date
+    encrypter = Encrypt.new(message, @key_generator.create_shift_keys(key) , @offset.create_offset(date))
     {
-      encryption: encryption,
+      encryption: encrypter.encrypt,
       key: key,
       date: date
     }
   end
 
-  def decrypt(ciphertext, key, date)
-
-  end
-
-  def keys
-
+  def decrypt(ciphertext, key, provided_date = nil)
+    date = provided_date.nil? ? @offset.generate_date : provided_date
+    decryptor = Decrypt.new(ciphertext, @key_generator.create_shift_keys(key) , @offset.create_offset(date))
+    {
+      decryption: decryptor.decrypt,
+      key: key,
+      date: date
+    }
   end
 end
